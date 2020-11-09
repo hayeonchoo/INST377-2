c249398e3ec0426adaefc4be13a8e9cf250ec877
hayeonchoo <70540047+hayeonchoo@users.noreply.github.com>
2020-11-08 19:33:23 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,10 @@
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
 const restaurants = [];
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => restaurants.push(...data))
 
 function findMatches(wordToMatch, restaurants) {
     return restaurants.filter(place => {
