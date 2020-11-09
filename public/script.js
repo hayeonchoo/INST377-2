12f1f04a7063f0e1b85819b92eb11e05533ef0f7
MLee314 <58196838+MLee314@users.noreply.github.com>
2020-11-08 19:26:19 -0500

    Update script.js
    adding more to js file

--- public/script.js
+++ public/script.js
@@ -1,8 +1,24 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-
 const restaurants = [];
 
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => console.log(data))
+function findMatches(wordToMatch, restaurants) {
+    return restaurants.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.zip.match(regex) || place.catgory.match(regex)
+    });
+}
 
+function displayMatches() {
+    const matchArray = findMatches(this.value, restaurants);
+    const html = matchArray.map(place => {
+        return `
+            
+        
+        `;
+    });
+}
+
+const serachInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
